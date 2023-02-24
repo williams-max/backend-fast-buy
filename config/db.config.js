2f2c2940bc94953fc342bb68213bf9811bde7586
Israel <almanzaisrael75@gmail.com>
2023-02-24 14:50:41 -0400

    update readme

--- config/db.config.js
+++ config/db.config.js
@@ -2,11 +2,16 @@
 const mysql = require('mysql2');
 //local mysql db connection
 const dbConn = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'publica',
+  database : 'node_mysql_crud_db'
 
+  /*remoto
   host: 'mysql-chevy.alwaysdata.net',
   user: 'chevy_free',
   password: 'adivinala',
-  database: 'chevy_dbfazt'
+  database: 'chevy_logsdb'*/
 });
 dbConn.connect(
     function (error) {
